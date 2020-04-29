735d37dd88c42b962ac4bdf2da44411c7c4962c7
Volodymyr Kopytin <volodymyr.kopytin@rebelmouse.com>
2020-04-29 18:47:08 +0200

    Remove failed bugfix

--- src/databindjs/core.ts
+++ src/databindjs/core.ts
@@ -444,7 +444,7 @@ const transferValue = (rootItem, state: IStateRecord, dataBinding: IDataBinding,
                 id: action.id
             });
 
-            if (!target.canWrite) {
+            if (false && !target.canWrite) {
                 queue.push((next, opId) => {
                     if (action.id === opId && target.aId === opId) {
                         return next();
